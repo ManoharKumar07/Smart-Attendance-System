2b8c08943faabaf2ba16a97db2299c9a186ed757
Manohar Kumar <manohar.csengineer@gmail.com>
2024-06-29 19:12:25 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,21 +3,27 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("http://127.0.0.1:8000/test/");
-      if (response) {
-        setData(response.data);
-        console.log("Data fetched successfully");
-      } else {
-        console.log("Error");
-      }
-    };
-    fetchData();
-  }, []);
+  // const [data, setData] = useState({});
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await axios.get("http://127.0.0.1:8000/test/");
+  //     if (response) {
+  //       setData(response.data);
+  //       console.log("Data fetched successfully");
+  //     } else {
+  //       console.log("Error");
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
-  return <>{data.message}</>;
+  return (
+    <>
+      <div className="">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
